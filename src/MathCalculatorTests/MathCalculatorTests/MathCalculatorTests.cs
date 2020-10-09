a0d5a135dc69b61da75eaecab42301e02241de49
TheMuver <via.zyn@ya.ru>
2020-10-09 15:21:39 +0300

    add new tests

--- src/MathCalculatorTests/MathCalculatorTests/MathCalculatorTests.cs
+++ src/MathCalculatorTests/MathCalculatorTests/MathCalculatorTests.cs
@@ -5,18 +5,16 @@ namespace MathCalculatorTests
 {
     public class MathCalculatorTests
     {
-        const int Five = 5;
-        const int Three = 3;
+        private const int Five = 5;
+        private const int Three = 3;
 
         [Fact]
         public void WhenSumFiveAndThreeThenGotEight()
         {
                 // Arrage
                 MathCalculator calculator = new MathCalculator();
-
                 // Act
                 var result = calculator.Sum(Five, Three);
-
                 // Assert
                 Assert.Equal(8, result);
         }
@@ -25,8 +23,34 @@ namespace MathCalculatorTests
         public void WhenSubFiveAndThreeGotTwo()
         {
             MathCalculator calculator = new MathCalculator();
+            
             var result = calculator.Sub(Five, Three);
+            
             Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void WhenSubFiveAndNegativeFiveGotNegativeOne()
+        {
+            MathCalculator calculator = new MathCalculator();
+            var result = calculator.Sub(Five, -Five);
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void WhenSumThreeAndNegativeThreeGotNull()
+        {
+            MathCalculator calculator = new MathCalculator();
+            var result = calculator.Sum(Three, -Three);
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void WhenSubThreeAndNullGotException()
+        {
+            MathCalculator calculator = new MathCalculator();
+            var result = calculator.Sub(Three, 0);
+            Assert.Equal(Three, result);        
         }
     }
 }

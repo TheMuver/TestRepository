7964511d05ffb704e4192a8f3ec6d16dd3f6f9eb
BiBa <crotow.tony@gmail.com>
2020-10-11 14:10:47 +0300

    overload sum

--- src/MathCalculatorTests/MathCalculatorTests/MathCalculator.cs
+++ src/MathCalculatorTests/MathCalculatorTests/MathCalculator.cs
@@ -8,6 +8,22 @@ namespace MathCalculatorTests
         {
             return a+b;
         }
+        public int Sum(string a, string b)
+        {
+            int num_1;
+            int num_2;
+
+            return Convert.ToInt32(a) + Convert.ToInt32(b);
+
+            if (int.TryParse(a, out num_1 ) && int.TryParse(b, out num_2 ))
+            {
+                return Convert.ToInt32(a) + Convert.ToInt32(b);
+            }
+            else  
+            {
+                return 0;
+            }
+        }
 
         public int Sub(int a, int b)
         {

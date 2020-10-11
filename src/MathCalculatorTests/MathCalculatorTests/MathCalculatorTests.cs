e5127ff778acacf92359caaa6a633da59da8bb71
TheMuver <via.zyn@ya.ru>
2020-10-11 14:24:30 +0300

    add some new tests

--- src/MathCalculatorTests/MathCalculatorTests/MathCalculatorTests.cs
+++ src/MathCalculatorTests/MathCalculatorTests/MathCalculatorTests.cs
@@ -24,5 +24,37 @@ namespace MathCalculatorTests
             var result = calculator.Sum(Five.ToString(), "wwww");
             Assert.Equal(0, result);
         }
+
+        [Fact]
+        public void WhenSumStringBeliberdaAndFiveGotNull()
+        {
+            MathCalculator calculator = new MathCalculator();
+            var result = calculator.Sum("www1", Five.ToString());
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void WhenSubStringFiveAndThreeGotTwo()
+        {
+            MathCalculator calculator = new MathCalculator();
+            var result = calculator.Sub(Five.ToString(), Three.ToString());
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void WhenSubStringBeliberdaAndFiveGotNull()
+        {
+            MathCalculator calculator = new MathCalculator();
+            var result = calculator.Sub(Five.ToString(), "ww112");
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void WhenSubStringFiveAndBeliberdaGotNull()
+        {
+            MathCalculator calculator = new MathCalculator();
+            var result = calculator.Sub( "ww112", Five.ToString());
+            Assert.Equal(0, result);
+        }
     }
 }

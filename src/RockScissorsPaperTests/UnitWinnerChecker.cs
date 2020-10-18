d413c53f4b835126c46f24c94ae2cb1309d376da
BiBa <crotow.tony@gmail.com>
2020-10-18 13:58:20 +0300

    add some tests

--- src/RockScissorsPaperTests/UnitWinnerChecker.cs
+++ src/RockScissorsPaperTests/UnitWinnerChecker.cs
@@ -8,6 +8,8 @@ namespace RockScissorsPaperTests
 {
     public class UnitWinnerChecker
     {
+
+        //Rock
         [Fact]
         public void WhenRockandPaperGotPaper()
         {
@@ -15,15 +17,25 @@ namespace RockScissorsPaperTests
             var result = winnerChecker.Play(rsp.Rock, rsp.Paper);
             Assert.Equal(2, result);
         }
-        
-        [Fact]
-        public void WnenInsertIntegerGotErr()
+
+        [Fact]      
+        public void WhenRockandRockGotDraw()
         {
             WinnerChecker winnerChecker = new WinnerChecker();
-            var result = winnerChecker.Play((rsp)5, (rsp)9);
-            Assert.Equal(-1, result);
+            var result = winnerChecker.Play(rsp.Rock, rsp.Rock);
+            Assert.Equal(0, result);
         }
 
+        [Fact]  
+        public void WhenRockandScissorsGotPaper()
+        {
+            WinnerChecker winnerChecker = new WinnerChecker();
+            var result = winnerChecker.Play(rsp.Rock, rsp.Scissors);
+            Assert.Equal(1, result);
+        }
+        //EndRock
+
+        //Scissors
         [Fact]
         public void WhenScissorsandScissorsGotDraw()
         {
@@ -33,11 +45,54 @@ namespace RockScissorsPaperTests
         }
 
         [Fact]
+        public void WhenScissorsandPaperGotDraw()
+        {
+            WinnerChecker winnerChecker = new WinnerChecker();
+            var result = winnerChecker.Play(rsp.Scissors, rsp.Paper);
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void WhenScissorsandRockGotDraw()
+        {
+            WinnerChecker winnerChecker = new WinnerChecker();
+            var result = winnerChecker.Play(rsp.Scissors, rsp.Rock);
+            Assert.Equal(2, result);
+        }
+        //EndScissors
+
+        //Paper
+        [Fact]
+        public void WhenPaperandPaperGotDraw()
+        {
+            WinnerChecker winnerChecker = new WinnerChecker();
+            var result = winnerChecker.Play(rsp.Paper, rsp.Paper);
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
         public void WhenPaperandRockGotPaper()
         {
             WinnerChecker winnerChecker = new WinnerChecker();
             var result = winnerChecker.Play(rsp.Paper, rsp.Rock);
             Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void WhenPaperandScissorsGotPaper()
+        {
+            WinnerChecker winnerChecker = new WinnerChecker();
+            var result = winnerChecker.Play(rsp.Paper, rsp.Scissors);
+            Assert.Equal(2, result);
+        }
+        //EndPaper
+
+        [Fact]
+        public void WnenInsertIntegerGotErr()
+        {
+            WinnerChecker winnerChecker = new WinnerChecker();
+            var result = winnerChecker.Play((rsp)5, (rsp)9);
+            Assert.Equal(-1, result);
         }
     }
 }

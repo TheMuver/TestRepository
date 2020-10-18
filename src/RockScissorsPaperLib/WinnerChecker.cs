ae606ef08a54c2e8e9e2a7b67340c81faa655bbe
TheMuver <via.zyn@ya.ru>
2020-10-18 13:13:18 +0300

    add some new tests for Play() v2

--- src/RockScissorsPaperLib/WinnerChecker.cs
+++ src/RockScissorsPaperLib/WinnerChecker.cs
@@ -13,27 +13,27 @@ namespace RockScissorsPaperLib
             {
                 case rsp.Paper:
                     if (second == rsp.Paper)
-                        return -1;
-                    else if (second == rsp.Rock)
                         return 0;
-                    else if (second == rsp.Scissors)
+                    else if (second == rsp.Rock)
                         return 1;
+                    else if (second == rsp.Scissors)
+                        return 2;
                     break;
                 case rsp.Rock:
                     if (second == rsp.Paper)
-                        return 1;
+                        return 2;
                     else if (second == rsp.Rock)
-                        return -1;
-                    else if (second == rsp.Scissors)
                         return 0;
+                    else if (second == rsp.Scissors)
+                        return 1;
                     break;
                 case rsp.Scissors:
                     if (second == rsp.Paper)
-                        return 0;
-                    else if (second == rsp.Rock)
                         return 1;
+                    else if (second == rsp.Rock)
+                        return 2;
                     else if (second == rsp.Scissors)
-                        return -1;
+                        return 0;
                     break;
                 default:
                     return -1;

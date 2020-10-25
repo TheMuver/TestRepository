4dce5ff8ea0f6a127c22967008bd49af891e8057
BiBa <crotow.tony@gmail.com>
2020-10-25 12:09:32 +0300

    add theory

--- src/RockScissorsPaperTests/UnitWinnerChecker.cs
+++ src/RockScissorsPaperTests/UnitWinnerChecker.cs
@@ -10,7 +10,7 @@ namespace RockScissorsPaperTests
 {
     public class UnitWinnerChecker
     {
-        [Fact]
+        /*[Fact]
         public void WnenInsertIntegerGotErr()
         {
             WinnerChecker winnerChecker = new WinnerChecker();
@@ -226,6 +226,36 @@ namespace RockScissorsPaperTests
             var result = winnerChecker.Play(rsp.Spoke, rsp.Lizard);
             Assert.Equal(2, result);
         }
-        //endSpoke
+        //endSpoke*/
+
+        [Fact]
+        public void CallsTests()
+        {
+            CheckTest(rsp.Rock, rsp.Paper, 2);
+        }
+
+        public void CheckTest(rsp player1, rsp player2, int exceptedResult)
+        {
+            AdditionalWinnerChecker winnerChecker = new AdditionalWinnerChecker();
+            var result = winnerChecker.Play(player1, player2);
+            Assert.Equal(exceptedResult, result);
+        }
+
+        [Theory]
+        [InlineData(rsp.Rock, rsp.Paper, 2)]
+        [InlineData(rsp.Rock, rsp.Scissors, 1)]
+        [InlineData(rsp.Rock, rsp.Rock, 0)]
+        [InlineData(rsp.Paper, rsp.Rock, 1)]
+        [InlineData(rsp.Paper, rsp.Scissors, 2)]
+        [InlineData(rsp.Paper, rsp.Paper, 0)]
+        [InlineData(rsp.Scissors, rsp.Rock, 2)]
+        [InlineData(rsp.Scissors, rsp.Paper, 1)]
+        [InlineData(rsp.Scissors, rsp.Scissors, 0)]
+        public void CheckTestTheory(rsp player1, rsp player2, int exceptedResult)
+        {
+            AdditionalWinnerChecker winnerChecker = new AdditionalWinnerChecker();
+            var result = winnerChecker.Play(player1, player2);
+            Assert.Equal(exceptedResult, result);
+        }
     }
 }

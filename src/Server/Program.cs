221c14e8b8b2f4c8159e283c02e6eb2d01cc0ef4
TheMuver <via.zyn@ya.ru>
2020-11-13 22:58:50 +0300

    already server

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Linq;
+using System.Net.WebSockets;
+using System.Security.Cryptography;
+using System.Runtime.CompilerServices;
 using System.Collections.Generic;
 using System.Threading;
 using System;
@@ -8,42 +11,46 @@ using System.Net.Sockets;
 using System.Text;
 using System.Collections;
 
+using RockScissorsPaperLib;
+
+
 namespace Server
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Server server = new Server();
+            server.WaitNewConnections();
         }
     }
 
     public class Server
     {
-        enum RequestType {None, Name, ChoiseSubject, Continue}
-
-        private Dictionary<string, TcpClient> clients;
+        private Dictionary<TcpClient, string> clients;
+        private Dictionary<TcpClient, rsp> players;
 
         public Server()
         {
-            clients = new Dictionary<string, TcpClient>();
+            clients = new Dictionary<TcpClient, string>();
+            players = new Dictionary<TcpClient, rsp>();
         }
 
         public void WaitNewConnections()
         {
+            (new Thread(new ThreadStart(Game))).Start(); // старт игрового обработчика
+
             TcpListener server = null;
             try
             {
                 Int32 port = 13000;
-                IPAddress addr = IPAddress.Parse("127.0.0.1");
+                IPAddress addr = IPAddress.Parse("26.129.184.182");
                 server = new TcpListener(addr, port);
                 server.Start();
-                Console.WriteLine("Server online");
+                Console.WriteLine("Server is online");
                 while (true)
                 {
                     TcpClient client = server.AcceptTcpClient();
-                    clients.Add((clients.Count+1).ToString(), client);
-                    Console.WriteLine("Connected new client");
                     (new Thread(new ParameterizedThreadStart(HandleConnection))).Start(client);
                 }
             }
@@ -60,17 +67,104 @@ namespace Server
         public void HandleConnection(Object c)
         {
             TcpClient client = c as TcpClient;
-            Console.WriteLine("New handler online");
+            Console.WriteLine("New handler is online");
+
+            Byte[] bytes = new byte[256];
+            String data = null;
+            NetworkStream stream = client.GetStream();
+            int i;
+
+            SendMessage(client, "name?"); // Запрос на имя
+            
+            while((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                data = Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("New request came in");
+                RequestProcessing(client, data);
+            }
         }
 
-        public void RequestProcessing(string mess)
+        public void Game()
         {
-            
+            Console.WriteLine("Game cycle is online");
+            while (true)
+            {
+                if (clients.Count < 2) // если не достаточно игроков для игры
+                    foreach (var x in clients.Keys)
+                        SendMessage(x, "waiting");
+                else if (players.Keys.Count < 2) // смотрим, все ли выбрали свои предметы
+                {
+                    foreach (var x in clients.Keys)
+                        if (!players.ContainsKey(x))
+                            SendMessage(x, "choise?");
+                }
+                else
+                {
+                    AdditionalWinnerChecker w = new AdditionalWinnerChecker();
+                    var answers = players.Values.ToArray(); // предметы
+                    TcpClient[] playerses = players.Keys.ToArray(); // клиенты
+                    int winner = w.Play(answers[0], answers[1])-1;
+                    foreach (var x in clients.Keys)
+                    {
+                        SendMessage(x, $"winner:{clients.Values.ToArray()[winner]}:{answers[winner]}");
+                    }
+                    break;
+                }
+                
+                Thread.Sleep(1000); // обработчик засыпает на секунду
+            }
+        }
+
+        public void RequestProcessing(TcpClient client, string mess)
+        {
+            String[] req = mess.Split(":");
+            switch(req[0])
+            {
+                case "name": // запись в клиенты
+                    clients.Add(client, req[1]);
+                    foreach (var x in clients.Keys)
+                        if (x != client)
+                            SendMessage(x, $"player:{req[1]}");
+                        Console.WriteLine($"New player is {req[1]}");
+                    break;
+                case "choise":
+                    rsp choice = rsp.Lizard;
+                    switch(req[1])
+                    {
+                        case "paper":
+                            choice = rsp.Paper;
+                            break;
+                        case "lizard":
+                            choice = rsp.Lizard;
+                            break;
+                        case "rock":
+                            choice = rsp.Rock;
+                            break;    
+                        case "scissors":
+                            choice = rsp.Scissors;
+                            break;
+                        case "spoke":
+                            choice = rsp.Spoke;
+                            break;
+                    }
+                    players.Add(client, choice);
+                    foreach (var x in clients.Keys)
+                        if (true/*x != client*/)
+                            SendMessage(x, $"choised:{clients[client]}");
+                    break;
+                case "":
+                    break;
+                default:
+                    Console.WriteLine($"Unknow request: {mess}");
+                    break;
+            }
         }
 
         public void SendMessage(TcpClient client, string mess)
         {
-
+            byte[] bytes = Encoding.ASCII.GetBytes(mess);
+            NetworkStream stream = client.GetStream();
+            stream.Write(bytes, 0, bytes.Length);
         }
     }
 }
